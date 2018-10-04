37418694d1f9a445563c362a5778c39793142bc7
Adam-Thain <Adam.r.thain@gmail.com>
2018-10-04 01:06:24 +0100

    Music Added
    Added Music Class

---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MusicPlayer : MonoBehaviour {
+    
+    #region Constructor
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    private void Awake()
+    {
+        SetupSingleton();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void SetupSingleton()
+    {
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+
+    }
+
+    #endregion
+}
